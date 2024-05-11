5ee750de04b975a5ca41456f393fa19a99a282c7
AhmadSaleh99 <saleehsale816@gmail.com>
2024-05-11 03:02:03 -0500

    animation

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,39 @@
+import Lottie from "lottie-react";
 import "./contact.css";
+import contactAnimation from "../../../public/animation/contact.json";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contactContainer">
+      <div className="contactHeader">
+        <span className="icon">icon</span>
+        <span className="contactTitle">Contact us</span>
+        <p className="contactDescription">
+          Contact me by sending an Email or scroll to top for my social media
+          links.{" "}
+        </p>
+      </div>
+      <div className="contactBody">
+        <div className="leftSide">
+          <form className="contactForm">
+            <label htmlFor="">Email address: </label>
+            <input type="email" />
+            <label htmlFor="">Message: </label>
+            <textarea rows="4" cols="50" />
+
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="rightSide">
+          <Lottie
+            // loop={false}
+            style={{ height: 355 }}
+            animationData={contactAnimation}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,18 @@
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footerContainer">
+      <ul className="navbarItems">
+        <li>About</li>
+        <li>Articles</li>
+        <li>Projects</li>
+        <li>Speaking</li>
+        <li>Contact</li>
+      </ul>
+      <div className="logo">logo</div>
+    </div>
+  );
 };
 
 export default Footer;
