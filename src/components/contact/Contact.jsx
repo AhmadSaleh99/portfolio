7dcb040fdfb234dd42aa8ddf1281cc0d8b63d470
AhmadSaleh99 <saleehsale816@gmail.com>
2024-07-07 14:59:14 -0500

    done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,12 @@ import Lottie from "lottie-react";
 import "./contact.css";
 import contactAnimation from "../../../public/animation/contact.json";
 import smallContactIcon from "../../../public/animation/smallContact.json";
+import doneAnimation from "../../../public/animation/done.json";
+import { ValidationError, useForm } from "@formspree/react";
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("xgvweqeq");
+
   return (
     <div className="contactContainer">
       <div className="contactHeader">
@@ -22,13 +26,47 @@ const Contact = () => {
       </div>
       <div className="contactBody">
         <div className="leftSide">
-          <form className="contactForm">
+          <form className="contactForm" onSubmit={handleSubmit}>
             <label htmlFor="">Email address: </label>
-            <input type="email" />
+            <input autoComplete="off" type="email" id="name" name="email" />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
             <label htmlFor="">Message: </label>
-            <textarea rows="4" cols="50" />
+            <textarea rows="4" cols="50" id="message" name="message" />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
 
-            <button type="submit">Send</button>
+            <button
+              className="submitButton"
+              type="submit"
+              disabled={state.submitting}
+            >
+              {state.submitting ? "sending" : "send"}
+            </button>
+            {state.succeeded && (
+              <p
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  fontSize: "18px",
+                  marginTop: "1.7rem",
+                }}
+              >
+                <Lottie
+                  loop={false}
+                  animationData={doneAnimation}
+                  style={{ height: 55 }}
+                />{" "}
+                Your message has been sent successfully.
+              </p>
+            )}
           </form>
         </div>
         <div className="rightSide">
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -18,79 +18,94 @@ const button = [
 const projects = [
   {
     id: 1,
-    title: "Front end",
-    img: "/public/images/test.png",
+    title: "Gym Exercises",
+    img: "/public/images/p8.png",
     category: "Front-End",
-    description: "project description",
-    reviewLink: "#",
-    gitLink: "#",
+    description: "",
+
+    reviewLink: "https://sage-meerkat-faedb8.netlify.app/",
+    gitLink: "https://github.com/AhmadSaleh99/gym-exercises",
   },
   {
     id: 2,
-    title: "full stack",
-    img: "/public/images/test.png",
+    title: "YouTube clone",
+    img: "/public/images/p9.png",
 
-    category: "Full-Stack",
-    description: "project description",
-    reviewLink: "#",
-    gitLink: "#",
+    category: "Front-End",
+    description:
+      "a stylish and user-friendly coffee shop website. Crafted with HTML, CSS, and JS, it brings the cozy charm of a coffee shop online. Browse through the menu, explore blends, and catch up on the latest promotions. A simple, yet delightful, experience for coffee lovers. Welcome to BeanBuzz Café – where web development meets the love for coffee",
+
+    reviewLink: "https://subtle-lolly-f3fd73.netlify.app/",
+    gitLink: "https://github.com/AhmadSaleh99/youTube-Clone",
   },
   {
     id: 3,
-    title: "Front end",
-    img: "/public/images/test.png",
+    title: "Todo List",
+    img: "/public/images/p10.png",
     category: "Front-End",
-    description: "project description",
-    reviewLink: "#",
-    gitLink: "#",
+    description:
+      "a stylish and user-friendly coffee shop website. Crafted with HTML, CSS, and JS, it brings the cozy charm of a coffee shop online. Browse through the menu, explore blends, and catch up on the latest promotions. A simple, yet delightful, experience for coffee lovers. Welcome to BeanBuzz Café – where web development meets the love for coffee",
+
+    reviewLink: "https://ahmadsaleh99.github.io/todo-list/",
+    gitLink: "https://github.com/AhmadSaleh99/todo-list",
   },
   {
     id: 4,
-    title: "full stack",
-    img: "/public/images/test.png",
+    title: "Simple Linkedin clone",
+    img: "/public/images/p2.png",
 
     category: "Full-Stack",
-    description: "project description",
-    reviewLink: "#",
-    gitLink: "#",
+    description:
+      "a simple Linkedin clone project built with React, Redux, and Firebase. Sign up, log in, and effortlessly share your thoughts through text-based posts. Elevate your professional presence in a user-friendly environment designed for simplicity and connectivity.The project brings together the power of React for a dynamic user interface, Redux for state management, and Firebase for secure authentication and real-time data storage",
+
+    reviewLink: "https://linkedin-clone-7ed09.web.app/",
+    gitLink: "https://github.com/AhmadSaleh99/linkedin-clone",
   },
   {
     id: 5,
-    title: "Front end",
-    img: "/public/images/test.png",
+    title: "simple netflix clone",
+    img: "/public/images/p4.png",
     category: "Front-End",
-    description: "project description",
-    reviewLink: "#",
-    gitLink: "#",
+    description:
+      " Introducing FlixHub – a React-based Netflix-inspired project that brings the world of movies to your fingertips. With Firebase authentication, dive into a personalized movie exploration experience. Browse through diverse categories, discover new releases, and read detailed movie descriptions.",
+
+    reviewLink: "https://netflix-clone-83a0f.web.app/",
+    gitLink: "https://github.com/AhmadSaleh99/netflix-clone",
   },
   {
     id: 6,
-    title: "full stack",
-    img: "/public/images/test.png",
+    title: "Leon agency",
+    img: "/public/images/p5.png",
 
     category: "Full-Stack",
-    description: "project description",
-    reviewLink: "#",
-    gitLink: "#",
+    description:
+      "A profitional design for an online agency called Leon with html, css, webfonts and js.",
+
+    reviewLink: "https://ahmadsaleh99.github.io/template-one/",
+    gitLink: "https://github.com/AhmadSaleh99/template-one",
   },
   {
     id: 7,
-    title: "Front end",
-    img: "/public/images/test.png",
+    title: "Coffee Shop",
+    img: "/public/images/p6.png",
     category: "Front-End",
-    description: "project description",
-    reviewLink: "#",
-    gitLink: "#",
+    description:
+      "a stylish and user-friendly coffee shop website. Crafted with HTML, CSS, and JS, it brings the cozy charm of a coffee shop online. Browse through the menu, explore blends, and catch up on the latest promotions. A simple, yet delightful, experience for coffee lovers. Welcome to BeanBuzz Café – where web development meets the love for coffee",
+
+    reviewLink: "https://ahmadsaleh99.github.io/Coffee-Shope/",
+    gitLink: "https://github.com/AhmadSaleh99/Coffee-Shope",
   },
   {
     id: 8,
-    title: "full stack",
-    img: "/public/images/test.png",
+    title: "GPT-3",
+    img: "/public/images/p7.png",
 
     category: "Full-Stack",
-    description: "project description",
-    reviewLink: "#",
-    gitLink: "#",
+    description:
+      "a stylish and user-friendly coffee shop website. Crafted with HTML, CSS, and JS, it brings the cozy charm of a coffee shop online. Browse through the menu, explore blends, and catch up on the latest promotions. A simple, yet delightful, experience for coffee lovers. Welcome to BeanBuzz Café – where web development meets the love for coffee",
+
+    reviewLink: "https://stupendous-froyo-3d8f4f.netlify.app/",
+    gitLink: "https://github.com/AhmadSaleh99/gpt_ash",
   },
 ];
 const Main = () => {
